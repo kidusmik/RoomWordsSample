012c1f123b15aa19e59a232350907ddc6924490c
kidusmik <kidusmik@gmail.com>
2020-05-09 02:33:05 +0300

    Added Database.

--- app/src/main/java/com/kidusmik/roomwordssample/Word.java
+++ app/src/main/java/com/kidusmik/roomwordssample/Word.java
@@ -1,10 +1,14 @@
 package com.kidusmik.roomwordssample;
 
 import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 @Entity(tableName = "word_table")
 public class Word {
+
+    @PrimaryKey @NonNull @ColumnInfo(name = "word")
     private String mWord;
 
     public Word(@NonNull String mWord) {
